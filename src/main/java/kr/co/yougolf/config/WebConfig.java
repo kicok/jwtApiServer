dbe2e21d9010cd880b48b69b4e634b535cddaa2b
kicok <kicoker@gmail.com>
2020-08-04 16:26:42 +0900

    delete todo

--- src/main/java/kr/co/yougolf/config/WebConfig.java
+++ src/main/java/kr/co/yougolf/config/WebConfig.java
@@ -1,6 +1,7 @@
 package kr.co.yougolf.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200");
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods(
+	            	HttpMethod.GET.name(),
+	            	HttpMethod.HEAD.name(),
+	            	HttpMethod.POST.name(),
+	            	HttpMethod.PUT.name(),
+	            	HttpMethod.DELETE.name()
+	           	);
+        
     }
 }
--- src/main/java/kr/co/yougolf/todo/TodoResource.java
+++ src/main/java/kr/co/yougolf/todo/TodoResource.java
@@ -3,6 +3,8 @@ package kr.co.yougolf.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,18 @@ public class TodoResource {
 	public List<Todo> getAllTodos(@PathVariable String username){
 		
 		return todoService.findAll();
+	}
+	
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(
+			@PathVariable String username, @PathVariable long id){
+		
+		Todo todo = todoService.deleteById(id);
+		if(todo!=null) { // delete todo success  
+			return ResponseEntity.noContent().build();
+		}
+		
+		//delete todo fail
+		return ResponseEntity.notFound().build();
 	}
 }
