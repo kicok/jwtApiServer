41708b0c4097e00af8778b746bcc9154aeeea3c6
kicok <kicoker@gmail.com>
2020-08-05 00:45:27 +0900

    make get todo for update page

--- src/main/java/kr/co/yougolf/todo/TodoResource.java
+++ src/main/java/kr/co/yougolf/todo/TodoResource.java
@@ -23,6 +23,12 @@ public class TodoResource {
 		return todoService.findAll();
 	}
 	
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo getTodos(@PathVariable String username, @PathVariable long id){
+		
+		return todoService.findById(id);
+	}
+	
 	@DeleteMapping("/users/{username}/todos/{id}")
 	public ResponseEntity<Void> deleteTodo(
 			@PathVariable String username, @PathVariable long id){
