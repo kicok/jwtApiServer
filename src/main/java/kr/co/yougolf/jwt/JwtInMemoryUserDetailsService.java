9c9290de0952635748305c94ff5e2390872b6903
kicok <kicoker@gmail.com>
2020-08-07 01:43:47 +0900

    add User in inMemory with jwt

--- src/main/java/kr/co/yougolf/jwt/JwtInMemoryUserDetailsService.java
+++ src/main/java/kr/co/yougolf/jwt/JwtInMemoryUserDetailsService.java
@@ -17,6 +17,10 @@ public class JwtInMemoryUserDetailsService implements UserDetailsService {
   static {
     inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
         "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
+    
+    inMemoryUserList.add(new JwtUserDetails(2L, "yougolf",
+    		"$2a$10$CiaJMPkGvE/PckHEiEUoq.T29t3tjJz0FHMaF5EsMohV6SBMyMO5u", "ROLE_USER_2"));
+    //$2a$10$CiaJMPkGvE/PckHEiEUoq.T29t3tjJz0FHMaF5EsMohV6SBMyMO5u
   }
 
   @Override
